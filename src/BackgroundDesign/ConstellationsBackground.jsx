a9efbdbe4a06249eb1f62a205938b54bbfcf1d68
Stacey Do <staceydo.9816@gmail.com>
2023-12-30 23:03:32 -0500

    update build for gh-pages

---
+++ src/BackgroundDesign/ConstellationsBackground.jsx
@@ -0,0 +1,31 @@
+// ConstellationsBackground.jsx
+
+import React from 'react';
+import './constellations.css';
+
+const ConstellationsBackground = () => {
+  const starCount = 50; // Adjust the number of stars as needed
+
+  const createStars = () => {
+    const stars = [];
+    for (let i = 0; i < starCount; i++) {
+      const sizeClass = `star-${Math.floor(Math.random() * 3) + 1}`;
+      const starStyle = {
+        left: `${Math.random() * 100}%`,
+        top: `${Math.random() * 100}%`,
+      };
+      stars.push(<div key={i} className={`constellation-star ${sizeClass}`} style={starStyle}></div>);
+    }
+    return stars;
+  };
+
+  return (
+    
+    <div className="constellations-container">
+      {createStars()}
+      <h1>Welcome to My Website!</h1>
+    </div>
+  );
+};
+
+export default ConstellationsBackground;
